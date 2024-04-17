d037fdbd995c4d1fd2cca3a250c1eaa85f4dad19
Daemon Jung <jjjssswww1006@gmail.com>
2024-04-17 11:06:49 +0900

    [setting] add material

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -87,4 +88,8 @@ dependencies {
 
     // Google Admob
     implementation("com.google.android.gms:play-services-ads:23.0.0")
+
+    // Google map
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.2.0")
 }
